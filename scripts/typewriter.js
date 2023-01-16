04e618aad53abf7b3fe2596ce0d1440fe0c4f280
SatvikGupta <satvik.sli@gmail.com>
2023-01-16 20:02:27 +0530

    Add github button and typewriting effect

---
+++ scripts/typewriter.js
@@ -0,0 +1,25 @@
+typewrite = async function () {
+  var finalText = "Hi. I'm Satvik.";
+  var i = 0;
+  var speed = 50;
+  var text = document.getElementById("name-heading");
+  text.innerHTML = "";
+  // var text2 = document.getElementById("name-heading2");
+  function typeWriter() {
+    if (i < finalText.length) {
+      text.innerHTML += finalText.charAt(i);
+      // text2.innerHTML += finalText.charAt(i);
+      i++;
+      setTimeout(typeWriter, speed);
+    } else {
+      i = 0;
+      // text2.innerHTML = "";
+      setTimeout(typewrite, 3000);
+      // text.innerHTML = " ";
+    }
+  }
+
+  setTimeout(typeWriter, 100);
+};
+
+typewrite();
