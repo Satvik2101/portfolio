72348f246898b6b02ab1f37a39b3654cfc890a74
SatvikGupta <satvik.sli@gmail.com>
2023-02-17 03:28:10 +0530

    Diff is calculated correctly now

---
+++ aws/scripts/filter.js
@@ -0,0 +1,69 @@
+const fs = require('fs');
+
+var changed_files = fs.readFileSync('./aws/changed_files', 'utf-8').toString().split("\n");
+
+
+//split by \n, or \r\n
+
+var toIgnore = fs.readFileSync('./aws/.awsignore', 'utf-8').toString().split("\r\n");
+
+function ignoreFile(file) {
+
+    for (igFile of toIgnore) {
+        if (file == igFile) {
+            return true;
+        }
+        //if file is in a directory, check if directory is in ignore list
+        // if (ignoreFile == 'aws') {
+        //     console.log(file);
+        //     console.log(file.indexOf(ignoreFile));
+        //     console.log(file[ignoreFile.length]);
+        // }
+        else if (file.indexOf(igFile) == 0 && file.length > igFile.length && file[igFile.length] == '/') {
+            return true;
+
+        } else {
+            // console.log(file, igFile);
+        }
+    }
+
+    return false;
+}
+var addedFiles = fs.readFileSync('./aws/added_files', 'utf-8').toString().split("\n");
+var finalAddedFiles = [];
+var finalChangedFiles = [];
+addedFiles.forEach(function (fileData) {
+    if (fileData.length == 0) { return; }
+    var isIgnored = ignoreFile(fileData);
+    if (!isIgnored) {
+        finalAddedFiles.push(fileData);
+    }
+});
+fs.writeFileSync('./aws/added_files', finalAddedFiles.join("\n"));
+
+
+// console.log(changed_files);
+changed_files.forEach(function (fileData) {
+    //split by tab
+    // console.log(fileData);
+    if (fileData.length == 0) { return; }
+    var fileData = fileData.split("\t");
+    var mode = fileData[0];
+    var file = fileData[1];
+
+    var isIgnored = ignoreFile(file);
+    if (!isIgnored) {
+        console.log(file);
+        if (mode == 'A') {
+            console.log('added');
+            fs.appendFileSync('./aws/added_files', file + "\n");
+        } else {
+            console.log('changed');
+            finalChangedFiles.push(file);
+        }
+        console.log();
+    }
+})
+
+
+fs.writeFileSync('./aws/changed_files', finalChangedFiles.join("\n"));
