d53c2ac13c820ed97577849bf28292a9496967f7
SatvikGupta <satvik.sli@gmail.com>
2023-04-06 00:57:54 +0530

    Start adding monolith notes

--- generators/home_gen.js
+++ generators/home_gen.js
@@ -1,3 +1,5 @@
+var generateNav = require("../helpers/navGen.js");
+
 var home = `
 <div id="bg_image">
 
@@ -7,30 +9,11 @@ var home = `
   <img src="./images/arc_reactor.png" alt="nav_bar_logo" />
 </a>
 </div>
-<div id="navbar">
+`
 
-      <div id="navbar_text">
-        .satvik
-      </div>
-      <div id="navbar_links">
-        <div class="navbar_link" id="home_link">
-          <a href="#home" target="_self">Home</a>
-        </div>
-        <div class="navbar_link" id="workexp_link">
-          <a href="#workexp" target="_self">
-            Work Experience
-          </a>
-        </div>
-        <div class=" navbar_link" id="projects_link">
-          <a href="#projects" target="_self">Projects</a>
-        </div>
-        <div class=" navbar_link" id="contacts_lini">
-        <a href="#contact" target="_self">Contact</a>
-      </div>
-        
-      </div>
-    </div>
+home += generateNav();
 
+home += `
 <div class="border_mask">
 
 
---
+++ helpers/navGen.js
@@ -0,0 +1,34 @@
+function generateNav() {
+    return `
+    
+    <div id="navbar" onclick="">
+
+    <div id="navbar_text">
+        .satvik
+    </div>
+    <div id="navbar_links">
+        <div class="navbar_link" id="home_link">
+            <a href="/#home" target="_self">Home</a>
+        </div>
+        <div class="navbar_link" id="workexp_link">
+            <a href="/#workexp" target="_self">
+                Work Experience
+            </a>
+        </div>
+        <div class=" navbar_link" id="projects_link">
+            <a href="/#projects" target="_self">Projects</a>
+        </div>
+        <div class=" navbar_link" id="notes_link">
+        <a href="/notes.html" target="_self">Notes</a>
+    </div>
+        <div class=" navbar_link" id="contacts_link">
+            <a href="/#contact" target="_self">Contact</a>
+        </div>
+      
+        
+    </div>
+</div>
+`
+}
+
+module.exports = generateNav;
