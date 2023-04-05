9a1b8f98a1625e94943e4d2b31946934445c2230
SatvikGupta <satvik.sli@gmail.com>
2023-04-06 02:36:23 +0530

    Add everything. Ready for upload/

--- generators/notes_gen.js
+++ generators/notes_gen.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 var navBar = require("../helpers/navGen")();
 
-const notes = ["OOSE", "CN", "DiS"];
+const notes = ["OOSE", "CN", "DiS", "MADF"];
 
 // Loop through each note in the notes array
 for (let i = 0; i < notes.length; i++) {
