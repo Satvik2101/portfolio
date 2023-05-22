a1c3f615ca635be4eff05888804fbf952798023f
SatvikGupta <satvik.sli@gmail.com>
2023-05-22 23:47:16 +0530

    Make add and reset analytics scripts recursive

--- helpers/resetAnalytics.js
+++ helpers/resetAnalytics.js
@@ -8,36 +8,36 @@ const ganalyticsTag = fs.readFileSync(ganalyticsTagFile, "utf8");
 const ganalyticsIdentifier = ganalyticsTag.match(/G-[A-Z0-9]{10}/)[0];
 
 // Go through all the html files in the root directory.
-const files = fs.readdirSync(".");
-for (const file of files) {
-    // If the file is an html file,
-    if (file.endsWith(".html")) {
-        // Read the file contents.
-        var fileContents = fs.readFileSync(file, "utf8");
 
-        // Check if the file contains the identifier.
-        if (fileContents.includes(ganalyticsIdentifier)) {
-            // If it doesn't, add the script to it and save the file.
-            //If file contains the script, remove it.
-            const ganalyticsTagIndex = fileContents.indexOf(ganalyticsTag);
-            console.log(ganalyticsTagIndex)
-            if (ganalyticsTagIndex != -1) {
-                fileContents = fileContents.slice(0, ganalyticsTagIndex) + fileContents.slice(ganalyticsTagIndex + ganalyticsTag.length);
-            }
+function removeTagInFolder(ogPath) {
+    console.log("removing in " + ogPath)
+    const files = fs.readdirSync("./" + ogPath);
+    for (const file of files) {
+        console.log(file)
+        const filePath = ogPath + "/" + file;
+        // If the file is an html file,
+        if (filePath.endsWith(".html")) {
+            // Read the file contents.
+            var fileContents = fs.readFileSync(filePath, "utf8");
 
-            fs.writeFileSync(file, fileContents, "utf8");
-        }
-    } else if (fs.statSync(file).isDirectory()) {
-        const filesInDir = fs.readdirSync(file);
-        for (const nestedFile of filesInDir) {
-            if (nestedFile.endsWith(".html")) {
-                var nestedFileContents = fs.readFileSync(file + "/" + nestedFile, "utf8");
-                if (!nestedFileContents.includes(ganalyticsIdentifier)) {
-                    const nestedHeadTagIndex = nestedFileContents.indexOf("</head>") + 7;
-                    nestedFileContents = nestedFileContents.slice(0, nestedHeadTagIndex) + ganalyticsTag + nestedFileContents.slice(nestedHeadTagIndex);
-                    fs.writeFileSync(file + "/" + nestedFile, nestedFileContents, "utf8");
+            // Check if the file contains the identifier.
+            if (fileContents.includes(ganalyticsIdentifier)) {
+                // If it doesn't, add the script to it and save the file.
+                //If file contains the script, remove it.
+                const ganalyticsTagIndex = fileContents.indexOf(ganalyticsTag);
+                console.log(ganalyticsTagIndex)
+                if (ganalyticsTagIndex != -1) {
+                    fileContents = fileContents.slice(0, ganalyticsTagIndex) + fileContents.slice(ganalyticsTagIndex + ganalyticsTag.length);
                 }
+
+                fs.writeFileSync(filePath, fileContents, "utf8");
             }
+        } else if (fs.statSync(filePath).isDirectory()) {
+            console.log("folder", file);
+            removeTagInFolder(filePath);
         }
     }
+
 }
+
+removeTagInFolder(".");
