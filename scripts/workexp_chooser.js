ba6e58034bc4f70117b5c38bb303b5d000e4dfcb
SatvikGupta <satvik.sli@gmail.com>
2023-12-27 01:50:29 +0530

    WorkExp changes - list of workexp shown and active one's details shown.

---
+++ scripts/workexp_chooser.js
@@ -0,0 +1,33 @@
+var chosen = 0;
+
+var choosers = document.getElementsByClassName("workexp_chooser_item");
+var workexp_cards = document.getElementsByClassName("workexp_card");
+
+var degs = [15, 185, 160, 40];
+
+var workexp = document.getElementById("workexp");
+function choose(i) {
+    console.log(i)
+    if (i == chosen) {
+        return;
+    }
+    choosers[chosen].classList.remove("workexp_chooser_item_selected");
+    choosers[i].classList.add("workexp_chooser_item_selected");
+    workexp_cards[chosen].classList.remove("workexp_card_selected");
+    workexp_cards[i].classList.add("workexp_card_selected");
+    chosen = i;
+
+    workexp.style.filter = "hue-rotate(" + degs[i] + "deg)";
+}
+
+//attach event listeners to buttons inside choosers[i]
+for (let i = 0; i < choosers.length; i++) {
+    let button = choosers[i].querySelector('button');
+    console.log(button)
+    if (button) {
+        button.addEventListener("click", function () {
+
+            choose(i);
+        });
+    }
+}
