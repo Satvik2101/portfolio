fcac83816d90986e858c5372d943d513dda198b7
SatvikGupta <satvik.sli@gmail.com>
2025-06-26 00:38:17 +0530

    fix notes

--- helpers/notes_gen.js
+++ helpers/notes_gen.js
@@ -1,10 +1,8 @@
 const fs = require('fs');
 // var navBar = require("../helpers/navGen")();
 
-const navBarClass = require("../lucid/out/global/NavBar")
 
-const navBar = new navBarClass.default().toString();
-html = require('./html-template-func');
+// html = require('./html-template-func');
 
 
 // first two arguments are node and the script name
@@ -27,23 +25,19 @@ for (let i = 0; i < notes.length; i++) {
         data = data.replace('<html', '<html ontouchmove=""');
         data = data.replace("styles.css", "../styles/notes_style.css")
 
-        // Add the navBar after the <body> tag
-        const bodyStartIndex = data.indexOf('<body>') + '<body>'.length;
-        const modifiedData = data.slice(0, bodyStartIndex) + navBar + data.slice(bodyStartIndex);
-
         // Add the PDF link after the author field
-        const navStartIndex = modifiedData.indexOf('<nav');
-        const navEndIndex = modifiedData.indexOf('</nav>');
+        const navStartIndex = data.indexOf('<nav');
+        const navEndIndex = data.indexOf('</nav>');
 
 
-        const nav = modifiedData.slice(navStartIndex, navEndIndex);
+        const nav = data.slice(navStartIndex, navEndIndex);
         // console.log(navStartIndex, nav)
 
         const navStartTagEnd = nav.indexOf('>') + 1;
         console.log(nav.slice(0, navStartTagEnd));
-        const pdfLink = html`<div class="pdf_link_container"><a class="pdf_link" href="/notes/pdfs/${note}.pdf">Download as PDF</a></div>`;
+        const pdfLink = `<div class="pdf_link_container"><a class="pdf_link" href="/notes/pdfs/${note}.pdf">Download as PDF</a></div>`;
         const modifiedNav = nav.slice(0, navStartTagEnd) + pdfLink + nav.slice(navStartTagEnd);
-        const finalData = modifiedData.slice(0, navStartIndex) + modifiedNav + modifiedData.slice(navEndIndex);
+        const finalData = data.slice(0, navStartIndex) + modifiedNav + data.slice(navEndIndex);
 
         // Write the modified HTML file back to disk
         fs.writeFile(htmlPath, finalData, 'utf-8', (err) => {
