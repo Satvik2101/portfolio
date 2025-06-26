75fb1874fba7cd8af1d1db6bbf381a7a6b9b39fd
SatvikGupta <satvik.sli@gmail.com>
2025-06-26 23:19:10 +0530

    refactor add analytics

--- helpers/addAnalytics.js
+++ helpers/addAnalytics.js
@@ -1,35 +1,25 @@
-var fs = require("fs");
+const fs = require("fs");
+const path = require("path");
 
-// Read the google analytics script from a file.
-const ganalyticsTagFile = `./ganalyticstag.txt`;
-const ganalyticsTag = fs.readFileSync(ganalyticsTagFile, "utf8");
-
-// Extract the google analytics identifier from the file data.
+const ganalyticsTag = fs.readFileSync("./ganalyticstag.txt", "utf8");
 const ganalyticsIdentifier = ganalyticsTag.match(/G-[A-Z0-9]{10}/)[0];
-function addTagInFolder(ogPath) {
-    const files = fs.readdirSync("./" + ogPath);
-    for (const file of files) {
-        // If the file is an html file,
-        const filePath = ogPath + "/" + file;
-        if (filePath.endsWith(".html")) {
-            // Read the file contents.
-            var fileContents = fs.readFileSync(filePath, "utf8");
 
-            // Check if the file contains the identifier.
-            if (!fileContents.includes(ganalyticsIdentifier)) {
-                // If it doesn't, add the script to it and save the file.
-                const headTagIndex = fileContents.indexOf("</head>");
-                fileContents = fileContents.slice(0, headTagIndex) + ganalyticsTag + fileContents.slice(headTagIndex);
-                fs.writeFileSync(filePath, fileContents, "utf8");
+function injectAnalyticsTags(dir) {
+    for (const entry of fs.readdirSync(dir)) {
+        const fullPath = path.join(dir, entry);
+        const stat = fs.statSync(fullPath);
+
+        if (stat.isDirectory()) {
+            injectAnalyticsTags(fullPath);
+        } else if (fullPath.endsWith(".html")) {
+            let content = fs.readFileSync(fullPath, "utf8");
+
+            if (!content.includes(ganalyticsIdentifier)) {
+                const updated = content.replace("</head>", ganalyticsTag + "</head>");
+                fs.writeFileSync(fullPath, updated, "utf8");
             }
-        } else if (fs.statSync(filePath).isDirectory()) {
-            if (filePath.includes("lucid")) {
-                continue
-            }
-            addTagInFolder(filePath);
         }
     }
-
 }
 
-addTagInFolder("./web");
+injectAnalyticsTags("./web");
