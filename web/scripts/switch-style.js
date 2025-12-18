9c5e38f01ee74d25de4c1c7f47cc34a6edfe380c
Satvik Gupta <satvik.sli@gmail.com>
2025-12-18 23:01:41 +0530

    Remove some, add some styles

--- web/scripts/switch-style.js
+++ web/scripts/switch-style.js
@@ -3,12 +3,9 @@ const themeToggle = document.getElementById('styleToggle');
 
 const themes = [
     'styles-classic.css',
-    'styles-modern.css',
     'styles-retro.css',
-    'styles-minimalist.css',
     'styles-serif.css',
     'styles-brutalist.css',
-    'styles-soft.css',
     'styles-pastel.css',
     'styles-manuscript.css',
     'styles-blueprint.css',
