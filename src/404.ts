0e01cb07e5c372fd986565d3226832fc6aa9bc48
Satvik Gupta <satvik.sli@gmail.com>
2025-12-18 23:00:54 +0530

    absolute path

--- src/404.ts
+++ src/404.ts
@@ -23,7 +23,7 @@ class Error404 extends Tag {
 
 
                 new H1("404"),
-                new Img().src("./assets/images/ducky.png").height("300px"),
+                new Img().src("https://www.satvikgupta.com/assets/images/ducky.png").height("300px"),
                 new P("Oops, we couldn't find the page you were looking for."),
                 new P("Here's a duck to help you debug."),
                 new P([
